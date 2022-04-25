6d5e2e409003ab7855ba253b092664dba057353a
Jie Chen <jiechenmc@gmail.com>
2022-04-25 09:07:03 -0400

    useData hook complete

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import TypeFlair from "./TypeFlair";
-//import useData from "../hooks/useData";
+import useData from "../hooks/useData";
 
 const Home = () => {
+  const data = useData("bulbasaur");
+  console.log(data);
   return (
     <div className="">
       <TypeFlair type="GRASS"></TypeFlair>
--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,34 +1,50 @@
-import { Pokemon, PokemonClient } from "pokenode-ts";
+import { useEffect, useState } from "react";
+import { Pokemon, ChainLink, MainClient } from "pokenode-ts";
 
-const useData = async () => {
-  const api = new PokemonClient();
+interface PokemonData extends Pokemon {
+  evolution_chain: ChainLink;
+}
 
-  await api
-    .getPokemonByName("bulbasaur")
-    .then((data) => {
-      let pokemon: Pokemon = {
-        id: data.id,
-        name: data.name,
-        base_experience: data.base_experience,
-        height: data.height,
-        is_default: data.is_default,
-        order: data.order,
-        weight: data.weight,
-        abilities: data.abilities,
-        forms: data.forms,
-        game_indices: data.game_indices,
-        held_items: data.held_items,
-        location_area_encounters: data.location_area_encounters,
-        moves: data.moves,
-        sprites: data.sprites,
-        species: data.species,
-        stats: data.stats,
-        types: data.types,
-        past_types: data.past_types,
-      };
-      return pokemon;
-    })
-    .catch((error) => console.error(error));
+const useData = (pokemonName: string) => {
+  const api = new MainClient({
+    cacheOptions: { maxAge: 60000, exclude: { query: false } },
+  });
+  const [pokemon, setPokemon] = useState<PokemonData>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let PokemonData = await api.pokemon.getPokemonByName(pokemonName);
+      let EvolutionData = await api.evolution.getEvolutionChainById(
+        PokemonData.id
+      );
+
+      setPokemon({
+        id: PokemonData.id,
+        name: PokemonData.name,
+        base_experience: PokemonData.base_experience,
+        height: PokemonData.height,
+        is_default: PokemonData.is_default,
+        order: PokemonData.order,
+        weight: PokemonData.weight,
+        abilities: PokemonData.abilities,
+        forms: PokemonData.forms,
+        game_indices: PokemonData.game_indices,
+        held_items: PokemonData.held_items,
+        location_area_encounters: PokemonData.location_area_encounters,
+        moves: PokemonData.moves,
+        sprites: PokemonData.sprites,
+        species: PokemonData.species,
+        stats: PokemonData.stats,
+        types: PokemonData.types,
+        past_types: PokemonData.past_types,
+        evolution_chain: EvolutionData.chain,
+      });
+    };
+
+    fetchData().catch((err) => console.log(err));
+  }, []);
+
+  return pokemon;
 };
 
 export default useData;
