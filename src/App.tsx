2aef13c6dab1a47c6baa4137e036af790465bbd9
Jie Chen <jiechenmc@gmail.com>
2022-05-23 22:22:32 -0400

    converting cra react-ts to vite react-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import Home from "./components/Home";
-
-const App = () => {
-  return <Home></Home>;
-};
+function App() {
+  return <div className="App"></div>;
+}
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
